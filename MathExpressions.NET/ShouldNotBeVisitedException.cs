690aa245dda71624fb3af5b000f4e3e1ac945c78
Ivan Kochurkin <kvanttt@gmail.com>
2018-01-01 15:39:01 +0300

    Added ShouldNotBeVisitedException.

---
+++ MathExpressions.NET/ShouldNotBeVisitedException.cs
@@ -0,0 +1,20 @@
+﻿using Antlr4.Runtime;
+using System;
+
+namespace MathExpressionsNET
+{
+	public class ShouldNotBeVisitedException : Exception
+	{
+		private readonly string _nodeDescription;
+
+		public ShouldNotBeVisitedException(ParserRuleContext context)
+		{
+			_nodeDescription = MathExprParser.ruleNames[context.RuleIndex];
+		}
+
+		public override string ToString()
+		{
+			return $"Node `{_nodeDescription}` should not be visited.";
+		}
+	}
+}
